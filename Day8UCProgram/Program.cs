616a2ea735106feda7555d0e530bc69e400b27b9
“Mnishthakur” <mnish771@gmail.com>
2023-04-16 14:08:46 +0530

    UC-1 added successfully

--- Day8UCProgram/Program.cs
+++ Day8UCProgram/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace Day8UCProgram;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Example usage
+        double x1 = 0;
+        double y1 = 0;
+        double x2 = 3;
+        double y2 = 4;
+
+        Line line = new Line(x1, y1, x2, y2);
+        double length = line.CalculateLength();
+        Console.WriteLine($"Length of the line: {length}");
     }
 }
-
